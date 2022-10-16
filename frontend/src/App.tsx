92f50a42a4e8599f7bb5db565ba2a601421f0889
sucodlaranja <joaoandre210@gmail.com>
2022-10-16 13:52:34 +0100

    login and register css done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Login from "./view/login/Login";
 import Registo from "./view/registro/Registo";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Route path="/" element={<div className="bg-orange-200">Hello</div>} />
       <Route path="/l" element={<div>HelloLLLLLLLLll</div>} />
       <Route path="/registo" element={<Registo />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
