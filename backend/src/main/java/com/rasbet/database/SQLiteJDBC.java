7def11082c916a01196cd7d75b8a7dc7e0d73e7e
rsaSantos <rs.elsaint@gmail.com>
2022-10-18 17:43:03 +0100

    test db connection

--- backend/src/main/java/com/rasbet/database/SQLiteJDBC.java
+++ backend/src/main/java/com/rasbet/database/SQLiteJDBC.java
@@ -9,10 +9,11 @@ public class SQLiteJDBC {
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:database/rasbet_db.db");
+            c.close();
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
-        }
+        }        
         System.out.println("Opened database successfully");
     }
 }
