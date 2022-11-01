18f0b17890dcd44d6881dcc766ccffa548311533
LCMJ21 <jorge.lima.ne@gmail.com>
2022-11-01 01:02:24 +0000

    specialist can add events

---
+++ backend/src/main/java/com/rasbet/backend/Exceptions/SportDoesNotExistExeption.java
@@ -0,0 +1,8 @@
+package com.rasbet.backend.Exceptions;
+
+public class SportDoesNotExistExeption extends Exception {
+    public SportDoesNotExistExeption(String message) {
+        super(message);
+    }
+
+}
