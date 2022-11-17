c447d03e9c658ca883701d3bc06a9a18ee2dc1b4
rsaSantos <rs.elsaint@gmail.com>
2022-11-17 19:50:11 +0000

    import rsa keys

---
+++ backend/src/main/java/com/rasbet/backend/Security/Config/RsaKeyProperties.java
@@ -0,0 +1,12 @@
+package com.rasbet.backend.Security.Config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+public record RsaKeyProperties(RSAPublicKey rsaPublicKey, RSAPrivateKey rsaPrivateKey)
+{
+
+}
