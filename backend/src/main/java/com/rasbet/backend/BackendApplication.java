569c09b568a6e2fe0efa756cdd2094d1f0baa2d6
rsaSantos <rs.elsaint@gmail.com>
2022-11-17 19:49:06 +0000

    enable config properties

--- backend/src/main/java/com/rasbet/backend/BackendApplication.java
+++ backend/src/main/java/com/rasbet/backend/BackendApplication.java
@@ -1,10 +1,13 @@
 package com.rasbet.backend;
 
+import com.rasbet.backend.Security.Config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rasbet.backend.GamesAPI.Updater;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class BackendApplication {
 
