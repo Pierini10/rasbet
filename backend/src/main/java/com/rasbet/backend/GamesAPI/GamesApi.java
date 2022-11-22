05d0c0b910686a599ecbcf837e5a3f44571dd3f1
LCMJ21 <jorge.lima.ne@gmail.com>
2022-11-22 18:07:52 +0000

    comment

--- backend/src/main/java/com/rasbet/backend/GamesAPI/GamesApi.java
+++ backend/src/main/java/com/rasbet/backend/GamesAPI/GamesApi.java
@@ -84,8 +84,12 @@ public class GamesApi {
 	}
 
 	public static List<Event> getallEvents() throws Exception {
-		Map<String, Pair<String, String>> sports_name = getSports();
+		
 		List<Event> events = new ArrayList<Event>();
+
+		// TODO REMOVER PARA TER UPDATES
+		/*
+		Map<String, Pair<String, String>> sports_name = getSports();
 
 		events.addAll(getEvents(GET_URL, sports_name.get("soccer_primeira_liga"),
 		true));
@@ -94,6 +98,7 @@ public class GamesApi {
 			events.addAll(getEvents(build_odds_sports_api_URL(sport, "eu"), sports_name.get(sport), false));
 			events.addAll(getEventsScores(build_scores_sports_api_URL(sport, "eu", "3")));
 		}
+		*/
 
 		return events;
 	}
