73e4d794c05438bbe5583dd5fb622e6eea2e4578
rsaSantos <rs.elsaint@gmail.com>
2022-11-23 10:43:21 +0000

    removed cross origin tag

--- backend/src/main/java/com/rasbet/backend/Security/AuthFacade.java
+++ backend/src/main/java/com/rasbet/backend/Security/AuthFacade.java
@@ -5,7 +5,6 @@ import java.security.Principal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +39,6 @@ public class AuthFacade {
             @ApiResponse(responseCode = "200", description = "Login successful"),
             @ApiResponse(responseCode = "400", description = "Could not login"),
             @ApiResponse(responseCode = "401", description = "Invalid credentials") })
-    @CrossOrigin(origins = "*")
     @PostMapping("/login")
     public String token(Authentication authentication) {
         LOG.debug("Token requested for user '{}'.", authentication.getName());
