4cd87d90bb60cd88c85998f06dfe036d39392ec2
LCMJ21 <jorge.lima.ne@gmail.com>
2022-11-26 09:43:55 +0000

    get Balance done

--- backend/src/main/java/com/rasbet/backend/Controller/WalletFacade.java
+++ backend/src/main/java/com/rasbet/backend/Controller/WalletFacade.java
@@ -3,18 +3,25 @@ package com.rasbet.backend.Controller;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import com.rasbet.backend.Security.Service.RasbetTokenDecoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.rasbet.backend.Database.TransactionDB;
+import com.rasbet.backend.Database.UserDB;
 import com.rasbet.backend.Entities.Transaction;
+import com.rasbet.backend.Entities.User;
 import com.rasbet.backend.Exceptions.NoAmountException;
 import com.rasbet.backend.Exceptions.NoMinimumValueException;
 import com.rasbet.backend.Exceptions.NoPromotionCodeException;
+import com.rasbet.backend.Security.Service.RasbetTokenDecoder;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -91,6 +98,33 @@ public class WalletFacade {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             throw new ResponseStatusException(
                     HttpStatus.INTERNAL_SERVER_ERROR, "SQLException", e);
+        }
+    }
+
+    /**
+     * Get balance.
+     * 
+     * @return Balance
+     */
+    @Operation(summary = "Get balance.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transaction was successful."),
+            @ApiResponse(responseCode = "400", description = "Transaction failed."),
+            @ApiResponse(responseCode = "500", description = "SQLException.") })
+    @PostMapping("/getBalance")
+    public double getBalance(
+            @RequestParam() double amount,
+            @RequestParam(required = false) String promotionCode,
+            @RequestParam(required = false) String method,
+            @RequestHeader("Authorization") String token)
+        {
+            token = RasbetTokenDecoder.parseToken(token);
+        try {
+            User user = UserDB.get_User(new RasbetTokenDecoder(token, jwtDecoder).getId());
+            return user.getBalance();
+        } catch (SQLException e) {
+
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "SQLException");
         }
     }
     
